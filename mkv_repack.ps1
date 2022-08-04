a5637ad4917046974be364f4a8ac9c001769715d
Michael Calabrese <77016291+calabresemic@users.noreply.github.com>
2022-08-04 10:44:09 -0800

    Update mkv_repack.ps1
    - added message to show that script is searching for files
    - changed wording from bad to unwanted
    - recon accounts for foreign films
    - added file size before and after
    - built json output file

--- mkv_repack.ps1
+++ mkv_repack.ps1
@@ -20,19 +20,24 @@ Function Language-Repack {
     }
 
     #Search for files
-    $files = Get-ChildItem -Path $path -Filter *.mkv -Recurse
+    Write-Host "Searching for files in $path"
+    [array]$files = Get-ChildItem -Path $path -Filter *.mkv -Recurse
 
     #Check for applicable files
     if ($files.Count -lt 1) {
-        Write-Warning "No mkv files found!"
+        Write-Warning "No .mkv files found!"
         Continue
     }
 
     #There are files to process if we've made it this far
+    Write-Host "$($files.count) .mkv files found!"
+    Write-Host ""
+    
+
     if($outputType -eq 'Recon'){
 
         #Initialize counter
-        $badfiles = 0
+        $badFiles = 0
 
         #Loop through and process files
         foreach($file in $files){
@@ -42,20 +47,34 @@ Function Language-Repack {
     
             #Find Audio and Subtitle Tracks
             [array]$audioTracks = $fileInfo.tracks | Where-Object {$_.type -eq 'audio'}
-            [array]$badAudioTracks = $audioTracks | Where-Object {$_.properties.language -ne 'eng'}
+            [array]$unwantedAudioTracks = $audioTracks | Where-Object {$_.properties.language -ne 'eng'}
 
             [array]$subtitleTracks = $fileInfo.tracks | Where-Object {$_.type -eq 'subtitles'}
-            [array]$badsubtitleTracks = $subtitleTracks | Where-Object {$_.properties.language -ne 'eng'}
+            [array]$unwantedSubtitleTracks = $subtitleTracks | Where-Object {$_.properties.language -ne 'eng'}
 
-            #Display message if bad tracks exist
-            if($badAudioTracks.Count -gt 0 -or $badsubtitleTracks.Count -gt 0) {
-                $badfiles ++
-                Write-Host "$($file.Name) has $($badAudioTracks.Count) bad audio tracks and $($badsubtitleTracks.Count) bad subtitle tracks."
+            #Display message if unwanted tracks exist
+            if($unwantedAudioTracks.Count -eq $audioTracks.Count){
+                
+                #Foreign film, ignore audio tracks.
+                if($unwantedSubtitleTracks.Count -gt 0) {
+                    $badFiles ++
+                    Write-Host "$($file.Name) has 0 unwanted audio track(s) and $($unwantedSubtitleTracks.Count) unwanted subtitle track(s)."
+                }
+
+            } else {
+            
+                #Display message if unwanted tracks exist
+                if($unwantedAudioTracks.Count -gt 0 -or $unwantedSubtitleTracks.Count -gt 0) {
+                    $badFiles ++
+                    Write-Host "$($file.Name) has $($unwantedAudioTracks.Count) unwanted audio track(s) and $($unwantedSubtitleTracks.Count) unwanted subtitle track(s)."
+                }
+
             }
         }
 
         #Display total info
-        Write-Host "Total bad files: $badfiles of $($files.Count)"
+        Write-Host ""
+        Write-Host "Total files with bad tracks: $badFiles of $($files.Count)"
 
     } else {
         
@@ -71,20 +90,28 @@ Function Language-Repack {
 
         }
 
+        $resultsJSON = [pscustomObject]@{
+            Date = Get-Date
+            Operation = $outputType
+            Path = $path
+            Results = @()
+        }
+
         #Loop through and process files
         foreach($file in $files){
+
             #Get file information
             $fileInfo = & $mkvMergePath -J $($file.FullName) | ConvertFrom-Json
     
             #Find Audio and Subtitle Tracks
             [array]$audioTracks = $fileInfo.tracks | Where-Object {$_.type -eq 'audio'}
-            [array]$badAudioTracks = $audioTracks | Where-Object {$_.properties.language -ne 'eng'}
+            [array]$unwantedAudioTracks = $audioTracks | Where-Object {$_.properties.language -ne 'eng'}
 
             [array]$subtitleTracks = $fileInfo.tracks | Where-Object {$_.type -eq 'subtitles'}
-            [array]$badsubtitleTracks = $subtitleTracks | Where-Object {$_.properties.language -ne 'eng'}
+            [array]$unwantedSubtitleTracks = $subtitleTracks | Where-Object {$_.properties.language -ne 'eng'}
 
-            #If there's no bad tracks to remove, skip file
-            if($badAudioTracks.Count -eq 0 -and $badsubtitleTracks.Count -eq 0) {
+            #If there's no unwanted tracks to remove, skip file
+            if($unwantedAudioTracks.Count -eq 0 -and $unwantedSubtitleTracks.Count -eq 0) {
 
                 Write-Host "$($file.Name) does not need to be processed. Skipping."
                 continue
@@ -92,6 +119,9 @@ Function Language-Repack {
             } else {
 
                 Write-Host "Processing file: $($file.Name)"
+
+                #Calculate Original File Size
+                $originalFileSize = [string]::Format("{0:0.00} MB",$file.Length/1MB)
 
                 if($outputType -eq 'Replace'){
 
@@ -112,28 +142,40 @@ Function Language-Repack {
             }
 
             #Find tracks to keep
-            [array]$audioToCopy = $audioTracks | Where-Object {$_ -notin $badAudioTracks}
-            [array]$subtitleToCopy = $subtitleTracks | Where-Object {$_ -notin $badsubtitleTracks}
+            [array]$audioToCopy = $audioTracks | Where-Object {$_ -notin $unwantedAudioTracks}
+            [array]$subtitleToCopy = $subtitleTracks | Where-Object {$_ -notin $unwantedSubtitleTracks}
 
-            #Assuming all files have at least an english track.. filtered before downloading
-            #This technically works with files with no english tracks as well, will make this actually nice later
+            #If no english tracks exist (foreign film) this will just not add the -a param and will default to copying all.
             if($audioToCopy.Count -gt 0){
+                
                 $cmd += " -a $($audioToCopy.id -join ',')"
+                [array]$removedAudioTracks = $unwantedAudioTracks
+
+            } else {
+                
+                #Foreign Film, adjust the output for results
+                $removedAudioTracks = @()
+
             }
 
             #If there are multiple audio tracks in english pick the "best" one
-            #Best in this case means most channels since Dolby should be at least 5.1(6) and AC-3 is 2
+            #Best in this case means most channels since Dolby should be at least 6 and AC-3 is 2
             #I have discovered that this sometimes only works by accident between multiple 6 channel tracks. Oh well. They'll both sound good I guess...
             if($audioToCopy.Count -gt 1){
                 $cmd += " --default-track-flag $(($audioToCopy | Sort-Object {$_.properties.audio_channels} -Descending | Select-Object -First 1).id):true"
-                $cmd += " -a $($audioToCopy.id -join ',')"
             }
 
             #Check for english subtitle to keep or remove all
             if($subtitleToCopy.Count -eq 0 -or $noSubs){
+
                 $cmd += ' -S'
+                [array]$removedSubtitleTracks = $subtitleTracks
+
             } else {
+
                 $cmd += " -s $($subtitleToCopy.id -join ',')"
+                [array]$removedSubtitleTracks = $unwantedSubtitleTracks
+            
             }
 
             #Append source file
@@ -141,6 +183,23 @@ Function Language-Repack {
 
             #Send commands to commandline
             cmd /c $cmd
+
+            #Calculate New File Size
+            $newFileSize = [string]::Format("{0:0.00} MB",(Get-Item -LiteralPath $sourceFile).Length/1MB)
+
+            $resultEntry = [pscustomobject]@{
+                FileName = $file.Name
+                OriginalSize = $originalFileSize
+                NewFile = $newFileName
+                NewFileSize = $newFileSize
+                AudioTracksRemoved = $removedAudioTracks
+                SubtitleTracksRemoved = $removedSubtitleTracks
+                SizeChange = [string]([double]$newFileSize.Replace(' MB','') - [double]$originalFileSize.Replace(' MB','')) + ' MB'
+            }
+
+            $resultsJSON.Results += $resultEntry
         }
+
+        $resultsJSON | ConvertTo-Json | Out-File "$backupPath\mkv_repack_results.json" -Force
     }
 }
